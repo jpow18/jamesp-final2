2f0b6a9cb732d2afa97d6256136a049ea02be5b7
James Pow <jbpow18@icloud.com>
2023-04-23 13:23:18 -0400

    feat: first commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    'https://glitch.com/edit/#!/deep-mini-kidney?path=.env%3A1%3A0',
+    'http://127.0.0.1:5500',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'https://dazzling-snickerdoodle-777101.netlify.app/'
+];
+
+module.exports = allowedOrigins;
