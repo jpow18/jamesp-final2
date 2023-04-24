6610d1dca3198d9cd629feba00782cb7729ef050
James Pow <jbpow18@icloud.com>
2023-04-24 16:16:17 -0400

    feat: first commit

---
+++ middleware/verifyStates.js
@@ -0,0 +1,15 @@
+const states = require('../model/statesData.json');
+const stateAbbreviations = new Array(50);
+
+for (i = 0; i < 50; i++) {
+  stateAbbreviations.push(states.code);
+}
+
+
+const verifyStates = (state) => {
+  if (stateAbbreviations.find(state)) {
+    return true;
+  } else {
+    return false;
+  }
+}
