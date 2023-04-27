a2c2d9f60e3e5ba08a085e35c6935fb86c7a257c
James Pow <jbpow18@icloud.com>
2023-04-27 18:29:05 -0400

    fix: cleanup

--- server.js
+++ server.js
@@ -4,25 +4,12 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
-const { logger } = require('./middleware/logEvents');
-const errorHandler = require('./middleware/errorHandler');
-const verifyJWT = require('./middleware/verifyJWT');
-const cookieParser = require('cookie-parser');
-const credentials = require('./middleware/credentials');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
 
-
 // Connect to MongoDB
 connectDB();
-
-// custom middleware logger
-app.use(logger);
-
-// Handle options credentials check - before CORS!
-// and fetch cookies credentials requirement
-app.use(credentials);
 
 // Cross Origin Resource Sharing
 app.use(cors(corsOptions));
@@ -33,20 +20,12 @@ app.use(express.urlencoded({ extended: false }));
 // built-in middleware for json 
 app.use(express.json());
 
-// //middleware for cookies
-// app.use(cookieParser());
-
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/views')));
+//app.use('/', express.static(path.join(__dirname, '/views')));
 
 // routes
 app.use('/', require('./routes/root'));
-// app.use('/register', require('./routes/register'));
-// app.use('/auth', require('./routes/auth'));
-// app.use('/refresh', require('./routes/refresh'));
-// app.use('/logout', require('./routes/logout'));
 
-//app.use(verifyJWT);
 app.use('/states', require('./routes/api/states'));
 
 app.all('*', (req, res) => {
@@ -60,7 +39,6 @@ app.all('*', (req, res) => {
     }
 });
 
-app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
