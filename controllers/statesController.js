3544bcdaa7ee5327a925206bdc898c50345a7d16
James Pow <jbpow18@icloud.com>
2023-04-26 20:45:01 -0400

    fix: corrected adjustedIndex statement

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -163,7 +163,7 @@ const patchFunfact = async (req, res) => {
   }
 
   // Subtract 1 from index to adjust for zero-based data array
-  const adjustedIndex = index - 1;
+  const adjustedIndex = req.body.index - 1;
 
   // Verify that stateCode is valid
   const stateCode = req.params.stateCode.toUpperCase();
