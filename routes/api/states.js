9c2f4e28ad6f76c4a249cf749ca33ccdab852311
James Pow <jbpow18@icloud.com>
2023-04-27 18:28:54 -0400

    fix: cleaup

--- routes/api/states.js
+++ routes/api/states.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const statesController = require('../../controllers/statesController');
-const ROLES_LIST = require('../../config/roles_list');
-const verifyRoles = require('../../middleware/verifyRoles');
 
 router.route('/')
     .get(statesController.getAllStates);
