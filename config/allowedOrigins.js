be6fdb45cf068170372ae8a81929ebe428a16a7c
James Pow <jbpow18@icloud.com>
2023-05-03 21:13:56 -0400

    fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,4 @@
 const allowedOrigins = [
-    'https://glitch.com/edit/#!/deep-mini-kidney?path=.env%3A1%3A0',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
